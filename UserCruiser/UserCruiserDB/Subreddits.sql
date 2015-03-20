a60cc84c7a398befcb85173a7384411c5d56a588
CrustyJew <mattiprice520@gmail.com>
2015-03-20 14:21:13 -0500

    DB work

--- UserCruiser/UserCruiserDB/Subreddits.sql
+++ UserCruiser/UserCruiserDB/Subreddits.sql
@@ -2,6 +2,7 @@
 (
 	[SubredditID] INT NOT NULL PRIMARY KEY, 
     [SubName] NVARCHAR(50) NOT NULL, 
-    [AppID] NVARCHAR(50) NOT NULL, 
-    [PrivateKey] NVARCHAR(50) NOT NULL
+    [AppID] NVARCHAR(25) NOT NULL, 
+    [PrivateKey] NVARCHAR(50) NOT NULL, 
+    [Active] BIT NOT NULL
 )
