9358cfd09dcde98fd06f3f118ae446bce36d1411
CrustyJew <mattiprice520@gmail.com>
2015-03-20 14:50:46 -0500

    Complete refactor, burn it to the ground

--- SnooNotes/UserCruiser/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace UserCruiser.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
