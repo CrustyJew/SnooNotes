f563d37084ddf4164640ab869720e215f012583b
CrustyJew <mattiprice520@gmail.com>
2015-05-21 16:50:17 -0500

    Chrome extension taking form

---
+++ SnooNotes/SnooNotesAddon/data/modules/SNChrome.js
@@ -0,0 +1,43 @@
+﻿function browserInit() {
+    (function (snBrowser) {
+        snBrowser.requstUserNotes = function(users){
+            chrome.runtime.sendMessage({"method":"requestUserNotes","users": users});
+        }
+        snBrowser.loggedIn = function () {
+            chrome.runtime.sendMessage({ "method": "loggedIn" });
+        }
+        //Listeners
+        chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+            switch (request.method){
+                case "gotUsersWithNotes":
+                    snUtil.setUsersWithNotes(request.users);
+                    break;
+                case "receiveUserNotes" :
+                    processEntries(request.notes); //SNLoad.js
+                    break;
+                case "updateUsersWithNotes" :
+                    snUtil.updateUsersWithNotes(request.req);
+                    break;
+                case "reinitWorker" :
+                    snUtil.reinitWorker();
+                    break;
+                case "newNoteExistingUser":
+                    newNoteExistingUser(request.req); //snoonotes.js
+                    break;
+                case "newNoteNewUser" :
+                    newNoteNewUser(request.req); //snoonotes.js
+                    break;
+                case "deleteNoteAndUser" :
+                    deleteNoteAndUser(request.req); //snoonotes.js
+                    break;
+                case "deleteNote" :
+                    deleteNote(request.req); //snoonotes.js
+                    break;
+                default:
+                    break;
+            }
+        });
+        
+    }(snBrowser = window.snUtil || {}));
+   
+}
