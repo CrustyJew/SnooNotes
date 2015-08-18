b6409e702477b3ba757940b35f489b3e67c4e20b
Meepster <mattiprice520@gmail.com>
2015-08-17 23:40:50 -0500

    More tweaks for pre-beta release

--- SnooNotes/SnooNotesAddon/data/modules/SNFirefox.js
+++ SnooNotes/SnooNotesAddon/data/modules/SNFirefox.js
@@ -36,7 +36,7 @@
             deleteNote(req); //snoonotes.js
         });
         self.port.on("setNoteTypeCSS", function (css) {
-            snUtil.NoteStyles.innerHTML = css;
+            snUtil.NoteStyles.textContent = css;
         });
         self.port.on("setNoteTypeJSON", function (json) {
             snUtil.NoteTypes = json;
