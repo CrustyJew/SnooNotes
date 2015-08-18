cb4d369a497607f16422a185e29a62c2c9d51148
CrustyJew <mattiprice520@gmail.com>
2015-08-18 17:05:52 -0500

    Start of angular hackery

---
+++ SnooNotes/SnooNotesAPI/app/app.js
@@ -0,0 +1,15 @@
+﻿
+angular.module('SnooNotes', [
+    'ngRoute'
+]).config(function ($routeProvider) {
+    'use strict';
+    $routeProvider
+        .when('/', {
+            templateUrl: 'Views/home.html',
+            controller: 'HomeCtrl',
+            controllerAs: 'home'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
