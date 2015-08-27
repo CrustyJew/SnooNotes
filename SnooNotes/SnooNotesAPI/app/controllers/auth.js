7e2ad2453157914f5cb5fa348744ccd262478cb9
CrustyJew <mattiprice520@gmail.com>
2015-08-27 16:12:28 -0500

    Changed auth pattern to allow for redirect so less of a hang during login
    Angular login and logout work properly

--- SnooNotes/SnooNotesAPI/app/controllers/auth.js
+++ SnooNotes/SnooNotesAPI/app/controllers/auth.js
@@ -2,6 +2,24 @@
        .module('SnooNotes')
        .controller('AuthCtrl', AuthCtrl);
 
-function AuthCtrl($scope, AuthFactory) {
+function AuthCtrl($scope, AuthFactory, $modalInstance) {
     $scope.currentUser = AuthFactory.currentUser;
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    }
+    $scope.openLoginWindow = function () {
+        var oauthwin = window.open('/Auth/DoLogin', 'SnooLogin', 'height=850px,width=850px');
+        oauthwin.focus();
+        setTimeout(function () { CheckLogin(oauthwin) }, 1500);
+    }
+
+    function CheckLogin(win) {
+        if (win == null || win.closed) {
+            $modalInstance.close();
+            AuthFactory.getCurrentUser();
+        }
+        else{
+            setTimeout(function () { CheckLogin(win) }, 1500);
+        }
+    }
 }
