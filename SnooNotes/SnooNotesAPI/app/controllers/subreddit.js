ea863e6967b620ab0290ba5c68460b92859df3d4
CrustyJew <mattiprice520@gmail.com>
2015-08-28 16:06:20 -0500

    UI Router working properly
    Added resolve to subreddit state

---
+++ SnooNotes/SnooNotesAPI/app/controllers/subreddit.js
@@ -0,0 +1,13 @@
+﻿angular
+       .module('SnooNotes')
+       .controller('SubredditCtrl', SubredditCtrl);
+
+function SubredditCtrl($scope,$stateParams, SubFactory){
+    var subSettings = SubFactory.getByName($stateParams.subName);
+    if (!subSettings) {
+        //TODO ERROR
+        $scope.sub = { subName: "ERROR" };
+    } else {
+        $scope.sub = subSettings;
+    }
+}
