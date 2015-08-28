cc8f6baeaa51f1aae1effe59ed0469c434d85497
CrustyJew <mattiprice520@gmail.com>
2015-08-28 10:30:45 -0500

    Include nav.js which kept getting lost

---
+++ SnooNotes/SnooNotesAPI/app/controllers/nav.js
@@ -0,0 +1,16 @@
+﻿angular
+       .module('SnooNotes')
+       .controller('NavCtrl', NavCtrl);
+
+function NavCtrl($scope, AuthFactory, $modal) {
+    $scope.currentUser = AuthFactory.currentUser;
+    $scope.logout = function () {
+        AuthFactory.logout();
+    }
+    $scope.login = function () {
+        var loginModal = $modal.open({
+            templateUrl: "loginModal.html",
+            controller: 'AuthCtrl'
+        });
+    }
+}
