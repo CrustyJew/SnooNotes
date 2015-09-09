2c36c1d525424c875858d9d29bb14f54aa296d86
Meepster <mattiprice520@hotmail.com>
2015-09-09 13:17:44 -0500

    check in missing files

---
+++ SnooNotes/SnooNotesAPI/RequestObjects/TBImportMapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnooNotesAPI.RequestObjects
+{
+    public class TBImportMapping
+    {
+        private const string tbNTNone = "none";
+        private const string tbNTGoodUser = "gooduser";
+        private const string tbNTAbuseWarn = "abusewarn";
+        private const string tbNTSpamWatch = "spamwatch";
+        private const string tbNTSpamWarn = "spamwarn";
+        private const string tbNTBan = "ban";
+        private const string tbNTPermBan = "permban";
+        private const string tbNTBotBan = "botban";
+
+        public int snNone { get; set; }
+    }
+}
