60b26906b7ef2a441963e3f71b5b9993647041d6
Meepster <mattiprice520@hotmail.com>
2015-09-09 13:17:44 -0500

    check in missing files

---
+++ SnooNotes/SnooNotesAPI/Utilities/TBNoteUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnooNotesAPI.Utilities
+{
+    public static class TBNoteUtils
+    {
+        public static List<Models.Note> ConvertTBNotesToSnooNotes(string subName, Dictionary<string,int> tbToSNMapping, List<RedditSharp.Things.tbUserNote> tbNotes)
+        {
+            var toReturn = tbNotes.Select(tb => new Models.Note { AppliesToUsername = tb.AppliesToUsername, Message = tb.Message, NoteTypeID = tbToSNMapping[tb.NoteType], Submitter = tb.Submitter, SubName = subName, Timestamp = tb.Timestamp, Url=tb.Url });
+            return toReturn.ToList();
+        }
+        public static List<Models.Note> GetNotesToAdd(List<Models.Note> tbNotes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
