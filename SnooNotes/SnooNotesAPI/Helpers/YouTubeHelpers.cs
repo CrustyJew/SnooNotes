c1f2419b44f3e80a95dbceb688d22a2a91caf0d4
mp0658 <matti.price@aureon.com>
2016-10-06 12:16:29 -0500

    Fix multiple encoded YouTube links to extract ID

--- SnooNotes/SnooNotesAPI/Helpers/YouTubeHelpers.cs
+++ SnooNotes/SnooNotesAPI/Helpers/YouTubeHelpers.cs
@@ -6,6 +6,11 @@ namespace SnooNotesAPI.Helpers {
         public static string ExtractVideoId( string url ) {
             string id = null;
             url = System.Net.WebUtility.UrlDecode( url ); //decode for attribution links
+            string ddecode = System.Net.WebUtility.UrlDecode( url );
+            while ( url != ddecode ) {
+                url = ddecode;
+                ddecode = System.Net.WebUtility.UrlDecode( url );
+            }
             var lowerUrl = url.ToLower();
             if ( lowerUrl.Contains( "youtube" ) ) {
                 //it's a YouTube link
