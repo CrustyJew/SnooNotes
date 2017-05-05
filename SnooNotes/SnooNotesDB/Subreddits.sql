128954963853e6f0ea417b197f274426f401457b
CrustyJew <mattiprice520@gmail.com>
2017-05-04 21:43:44 -0500

    Fix column name6

--- SnooNotes/SnooNotesDB/Subreddits.sql
+++ SnooNotes/SnooNotesDB/Subreddits.sql
@@ -3,7 +3,7 @@
 	[SubredditID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
     [SubName] NVARCHAR(50) NOT NULL UNIQUE CLUSTERED, 
     [Active] BIT NOT NULL, 
-	[SentinelEnabled] BIT NOT NULL DEFAULT 0,
+	[SentinelActive] BIT NOT NULL DEFAULT 0,
     [DirtbagUrl] NVARCHAR(500) NULL, 
     [DirtbagUsername] NVARCHAR(100) NULL, 
     [DirtbagPassword] NVARCHAR(100) NULL 
