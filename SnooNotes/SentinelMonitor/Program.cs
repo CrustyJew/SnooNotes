bd8a2fe78d1fbea8a9b202447ebffbf318836688
CrustyJew <mattiprice520@gmail.com>
2017-07-10 09:35:54 -0500

    Fix for azure webjob

--- SnooNotes/SentinelMonitor/Program.cs
+++ SnooNotes/SentinelMonitor/Program.cs
@@ -16,7 +16,7 @@ namespace SentinelMonitor
         {
             var builder = new ConfigurationBuilder()
             .SetBasePath(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location))
-            .AddJsonFile("appsettings.json").AddEnvironmentVariables();
+            .AddJsonFile("appsettings.json",true).AddEnvironmentVariables();
 
             Config = builder.Build();
 
